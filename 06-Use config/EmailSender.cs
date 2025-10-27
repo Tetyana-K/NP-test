f2e559f7e2213c53aecdea61ef7f908f4bb6aa08
Tetyana-K <tanya.kyryk@gmail.com>
2025-10-27 20:21:52 +0200

    add use config project

---
+++ 06-Use config/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration; // Додай цей простір імен
+
+namespace _06_Use_config
+{
+    public class SmtpSettings
+    {
+        // Використовуємо ConfigurationManager для отримання налаштувань з App.config
+        public static string Server => ConfigurationManager.AppSettings["SmtpServer"]; // Отримуємо значення SmtpServer 
+        public static int Port => int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+        public static bool EnableSsl => bool.Parse(ConfigurationManager.AppSettings["EnableSsl"]);
+    }
+}
---
+++ 06-Use config/Program.cs
@@ -0,0 +1,26 @@
+﻿using _06_Use_config;
+using System.Net;
+using System.Net.Mail;
+
+string from = "your_email@gmail.com"; // ваша пошта
+string password = "your_app_password"; // ваш пароль додатка
+string to = "recepient@gmail.com"; // пошта отримувача
+string subject = "Тестовий лист  (SMTP)";
+string body = "Привіт! Це тестове повідомлення з C# (SMTP) з використанням конфігурації.";
+SendEmail(from, password, to, subject, body);
+
+
+
+void SendEmail(string from, string password, string to, string subject, string body)
+{
+    using var smtp = new SmtpClient
+    {
+        Host = SmtpSettings.Server,
+        Port = SmtpSettings.Port,
+        EnableSsl = SmtpSettings.EnableSsl,
+        Credentials = new NetworkCredential(from, password)
+    };
+
+    var message = new MailMessage(from, to, subject, body);
+    smtp.Send(message);
+}
